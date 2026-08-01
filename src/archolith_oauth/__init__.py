0cfdb464451f85a803e9a391bb93e74278630950
ctharvey <ctharvey@me.com>
2026-08-01 15:14:07 -0500

    feat: expose reusable OAuth package API

---
+++ src/archolith_oauth/__init__.py
@@ -0,0 +1,43 @@
+"""Reusable OAuth 2.1 building blocks for Archolith services."""
+
+from .config import AuthorizationServerConfig, ResourceServerConfig
+from .key_store import SigningKeyStore
+from .metadata import authorization_server_metadata, protected_resource_metadata
+from .models import AuthCodeRecord, AuthorizationGrant, OAuthClient, OAuthPrincipal
+from .pkce import create_verifier, s256_challenge, verify_s256
+from .registration import (
+    ClientMetadataError,
+    register_public_client,
+    validate_authorization_request,
+)
+from .stores import AuthorizationCodeStore, OAuthClientStore
+from .tokens import TokenIssuer, TokenResponse, exchange_authorization_code
+from .verifier import AccessTokenVerifier, OAuthAuthenticationError, extract_scopes
+
+__all__ = [
+    "AccessTokenVerifier",
+    "AuthCodeRecord",
+    "AuthorizationCodeStore",
+    "AuthorizationGrant",
+    "AuthorizationServerConfig",
+    "ClientMetadataError",
+    "OAuthAuthenticationError",
+    "OAuthClient",
+    "OAuthClientStore",
+    "OAuthPrincipal",
+    "ResourceServerConfig",
+    "SigningKeyStore",
+    "TokenIssuer",
+    "TokenResponse",
+    "authorization_server_metadata",
+    "create_verifier",
+    "exchange_authorization_code",
+    "extract_scopes",
+    "protected_resource_metadata",
+    "register_public_client",
+    "s256_challenge",
+    "validate_authorization_request",
+    "verify_s256",
+]
+
+__version__ = "0.1.0"
